a77937afbeee4bc40f9b01d57e3f2b35af768b6c
Martti Mourujarvi <marttimourujarvi@gmail.com>
2026-08-25 05:38:34 +0300

    initial nvim config

---
+++ nvim/lua/custom/plugins/claude.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'greggh/claude-code.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      require('claude-code').setup {
+        keymaps = {
+          toggle = {
+            normal = '<leader>cc',
+            terminal = '<leader>cc',
+          },
+        },
+      }
+    end,
+  },
+}
